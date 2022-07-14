6bc7da72993917017a268ff4fb63be35cd19e83f
Diogo Baltazar <ti3@ballke.com.br>
2022-07-14 14:20:26 -0300

    projeto beta

---
+++ README.md
@@ -0,0 +1,51 @@
+<p align="center">
+    <img width="800" src="logo-uffs.png" title="Logo do projeto"><br />
+    <img src="https://img.shields.io/maintenance/yes/2022?style=for-the-badge" title="Status do projeto">
+</p>
+
+# Bot R.U UFFS
+
+Bot que retorna cardápio r.u de todos os campus via telegram.
+
+## ✨ Features
+
+Ao invocar `/cardapio` o bot retorna opções para acessar o cardápio do dia
+
+* ✔️ Fácil integração;
+* 🥢 Poucas dependências;
+* 🎨 Utiliza um template lindo para organizar o `README`;
+
+## 🚀 Começando
+
+### 1. Primeiro passo para começar
+
+
+Instale as dependencias do projeto com:
+```
+pip install -r requirements.txt
+```
+
+Crie um arquivo `.env` e adicione suas keys do telegram.
+
+## 🤝 Contribua
+
+Sua ajuda é muito bem-vinda, independente da forma! Confira o arquivo [CONTRIBUTING.md](CONTRIBUTING.md) para conhecer todas as formas de contribuir com o projeto. Por exemplo, [sugerir uma nova funcionalidade](https://github.com/ccuffs/template/issues/new?assignees=&labels=&template=feature_request.md&title=), [reportar um problema/bug](https://github.com/ccuffs/template/issues/new?assignees=&labels=bug&template=bug_report.md&title=), [enviar um pull request](https://github.com/ccuffs/hacktoberfest/blob/master/docs/tutorial-pull-request.md), ou simplemente utilizar o projeto e comentar sua experiência.
+
+Veja o arquivo [ROADMAP.md](ROADMAP.md) para ter uma ideia de como o projeto deve evoluir.
+
+
+## 🎫 Licença
+
+Esse projeto é licenciado nos termos da licença open-source [MIT](https://choosealicense.com/licenses/mit) e está disponível de graça.
+
+## 🧬 Changelog
+
+Veja todas as alterações desse projeto no arquivo [CHANGELOG.md](CHANGELOG.md).
+
+## 🧪 Projetos semelhates
+
+Este projeto apresenta de forma amigável o cardápio do ru que é adquirido em:
+
+* [apiRuUffs](https://github.com/mascDriver/apiRuUffs)
+#### App Android: 
+* [App RU UFFS](https://github.com/mascDriver/app_ru_uffs)
---
+++ main.py
@@ -0,0 +1,122 @@
+from os import getenv
+
+import httpx
+from dotenv import load_dotenv
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from uvloop import install
+
+install()
+load_dotenv()
+
+app = Client(
+    'ruuffs_bot',
+    api_id=getenv('TELEGRAM_API_ID'),
+    api_hash=getenv('TELEGRAM_API_HASH'),
+    bot_token=getenv('TELEGRAM_BOT_TOKEN')
+)
+
+
+@app.on_callback_query()
+async def callback(client, callback_query):
+    pages = {
+        'campus': {
+            'chapeco': InlineKeyboardButton('Chapecó', callback_data='chapeco'),
+            'realeza': InlineKeyboardButton('Realeza', callback_data='realeza'),
+            'cerro_largo': InlineKeyboardButton('Cerro Largo', callback_data='cerro_largo'),
+            'laranjeiras_do_sul': InlineKeyboardButton('Laranjeiras do Sul', callback_data='laranjeiras_do_sul'),
+            'texto': 'Escolha o campus 📖'
+        },
+        'chapeco': {
+            'segunda': InlineKeyboardButton('Segunda', callback_data='0'),
+            'terca': InlineKeyboardButton('Terça', callback_data='1'),
+            'quarta': InlineKeyboardButton('Quarta', callback_data='2'),
+            'quinta': InlineKeyboardButton('Quinta', callback_data='3'),
+            'sexta': InlineKeyboardButton('Sexta', callback_data='4'),
+            'anterior': InlineKeyboardButton('Voltar pro Campus', callback_data='campus'),
+            'texto': 'Chapecó escolha o dia 📅'
+        },
+        'realeza': {
+            'segunda': InlineKeyboardButton('Segunda', callback_data='0'),
+            'terca': InlineKeyboardButton('Terça', callback_data='1'),
+            'quarta': InlineKeyboardButton('Quarta', callback_data='2'),
+            'quinta': InlineKeyboardButton('Quinta', callback_data='3'),
+            'sexta': InlineKeyboardButton('Sexta', callback_data='4'),
+            'anterior': InlineKeyboardButton('Voltar pro Campus', callback_data='campus'),
+            'texto': 'Realeza escolha o dia 📅'
+        },
+        'cerro_largo': {
+            'segunda': InlineKeyboardButton('Segunda', callback_data='0'),
+            'terca': InlineKeyboardButton('Terça', callback_data='1'),
+            'quarta': InlineKeyboardButton('Quarta', callback_data='2'),
+            'quinta': InlineKeyboardButton('Quinta', callback_data='3'),
+            'sexta': InlineKeyboardButton('Sexta', callback_data='4'),
+            'anterior': InlineKeyboardButton('Voltar pro Campus', callback_data='campus'),
+            'texto': 'Cerro Largo escolha o dia 📅'
+        },
+        'laranjeiras_do_sul': {
+            'segunda': InlineKeyboardButton('Segunda', callback_data='0'),
+            'terca': InlineKeyboardButton('Terça', callback_data='1'),
+            'quarta': InlineKeyboardButton('Quarta', callback_data='2'),
+            'quinta': InlineKeyboardButton('Quinta', callback_data='3'),
+            'sexta': InlineKeyboardButton('Sexta', callback_data='4'),
+            'anterior': InlineKeyboardButton('Voltar pro Campus', callback_data='campus'),
+            'texto': 'Laranjeiras do Sul escolha o dia 📅'
+        }
+    }
+    if callback_query.data in pages:
+        page = pages[callback_query.data]
+        txt = page.pop('texto')
+        inline_markup = InlineKeyboardMarkup(
+            [list(page.values())]
+        )
+        await callback_query.edit_message_text(txt, reply_markup=inline_markup)
+    else:
+        await callback_query.edit_message_text('**Aguarde, carregando cardapio 🔄 🔄 🔄**')
+        result = httpx.get(
+            f"https://api-ru-uffs.herokuapp.com/campus/{callback_query.message.text.split('escolha')[0].strip()}/dia/{callback_query.data}",
+            timeout=15).json()
+        result = result['cardapios'][0]
+        await callback_query.edit_message_text(f"""
+        
+        🗓️ {result['dia']}
+        🥗 {result['salada']}
+        🥗 {result['salada1']}
+        🥗 {result['salada2']}
+        🍚 {result['graos']}
+        🍙 {result['graos1']}
+        🍟 {result['acompanhamento']}
+        🥩 {result['mistura']}
+        🥦 {result['mistura_vegana']}
+        🍩 {result['sobremesa']}
+        """)
+
+
+@app.on_message(filters.command('cardapio'))
+async def callbacks(client, message):
+    inline_markup = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton('Cardápio', callback_data='campus')
+            ]
+        ]
+    )
+    await message.reply('**Escolha uma opção 🔗 **', reply_markup=inline_markup)
+
+
+@app.on_message(filters.command('help') | filters.command('start'))
+async def help_command(client, message):
+    await message.reply(
+        'Esse é o menu para pedir ajuda!\n'
+        'Use **/start** para iniciar o bot!\n'
+        'Use **/help** para pedir ajuda!\n'
+        'Use **/cardapio** para ver o cardápio\n'
+    )
+
+
+@app.on_message()
+async def messages(client, message):
+    await message.reply(f"Não entendi o {message.text}")
+
+
+app.run()
